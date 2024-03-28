bed1b99278a358cd83bc5c3e17351a75bb36eb8f
ANKIT1149 <140632363+ANKIT1149@users.noreply.github.com>
2024-03-28 13:52:00 +0530

    Client page setup completed

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+// import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Home, About, Profile, Signin,  Signup} from "../src/Pages/Index";
+const App = () => {
+  
+  return (
+    <BrowserRouter>
+       <Routes>
+           <Route path="/" element={<Home />} />
+           <Route path="/sign-in" element={<Signin />} />
+           <Route path="/sign-up" element={<Signup />} />
+           <Route path="/profile" element={<Profile />} />
+           <Route path="/about" element={<About />} />
+       </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ client/src/Pages/Index.js
@@ -0,0 +1,5 @@
+export { default as Home } from "./Home";
+export { default as Profile } from "./Profile.jsx";
+export { default as About } from "./About.jsx";
+export { default as Signin } from "./Signin.jsx";
+export { default as Signup } from "./Signup.jsx";
