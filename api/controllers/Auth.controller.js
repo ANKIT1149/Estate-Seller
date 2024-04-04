2367db24738164b73bc14bab7633a6a3f832bca3
ANKIT1149 <140632363+ANKIT1149@users.noreply.github.com>
2024-04-04 13:55:12 +0530

    complete signup with function

--- api/controllers/Auth.controller.js
+++ api/controllers/Auth.controller.js
@@ -4,8 +4,8 @@ import bcryptjs from "bcryptjs";
 
 export const Signup = async (req, res, next) => {
      const {username, email, password, phone} = req.body;
-     const hashedpassword = bcryptjs.hashSync(password, 10);
-     const newUser = new User({username, email, password: hashedpassword, phone});
+    //  const hashedpassword = bcryptjs.hashSync(password, 10);
+     const newUser = new User({username, email, password, phone});
        try {
             await newUser.save();
             res.status(201).json("User Created Successfully");
