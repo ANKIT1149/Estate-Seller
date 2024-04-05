28265c35095659f38c29e88b83ece4ff7c0aba3c
ANKIT1149 <140632363+ANKIT1149@users.noreply.github.com>
2024-04-05 20:18:02 +0530

    Sign in page routes created

--- api/Routes/Auth.routes.js
+++ api/Routes/Auth.routes.js
@@ -1,13 +1,10 @@
 import express from "express";
-
-import { signin, Signup } from "../controllers/Auth.controller.js";
 import { Signin, Signup } from "../controllers/Auth.controller.js";
+
 
 const AuthRouter = express.Router();
 
 AuthRouter.post("/signup", Signup);
-
-AuthRouter.post("/signin", signin)
 
 AuthRouter.post("/signin", Signin)
 
