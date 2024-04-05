fe1818a97e2f43747921fb81e5cca1db1693c668
ANKIT1149 <140632363+ANKIT1149@users.noreply.github.com>
2024-04-05 19:54:59 +0530

    error solved

--- api/Routes/Auth.routes.js
+++ api/Routes/Auth.routes.js
@@ -1,31 +1,11 @@
 import express from "express";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { Signin, Signup } from "../controllers/Auth.controller.js";
-=======
-import { signin, Signup } from "../controllers/Auth.controller.js";
->>>>>>> dev_Aryansh
-=======
-
-import { signin, Signup } from "../controllers/Auth.controller.js";
-import { Signin, Signup } from "../controllers/Auth.controller.js";
->>>>>>> db20cabcd2a7431600925157887fc4cb5d4de0b4
 
 const AuthRouter = express.Router();
 
 AuthRouter.post("/signup", Signup);
-<<<<<<< HEAD
-<<<<<<< HEAD
-AuthRouter.post("/signin", Signin)
-=======
-AuthRouter.post("/signin", signin)
->>>>>>> dev_Aryansh
-=======
-
-AuthRouter.post("/signin", signin)
 
 AuthRouter.post("/signin", Signin)
 
->>>>>>> db20cabcd2a7431600925157887fc4cb5d4de0b4
 
 export default AuthRouter;
